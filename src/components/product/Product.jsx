62ccaee432ab4ba4b581e4651a4668824d1efb8e
Bayshakhe Pramanik <bayshakhepramanik@gmail.com>
2023-03-27 20:28:30 +0600

    added product

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -5,12 +5,15 @@ const Product = (props) => {
     const {img, name, seller, price, ratings} = props.product
     // console.log(props.product)
     return (
-        <div className='card'>
+        <div className='product'>
             <img src={img} alt="" />
-            <h4>{name}</h4>
-            <h5>Price: ${price}</h5>
-            <p>Manufacturer: {seller}</p>
-            <p>Rating: {ratings}</p>
+            <div className="product-info">
+                <h6 className='product-name'>{name}</h6>
+                <h5 className='product-price'>Price: ${price}</h5>
+                <p>Manufacturer: {seller}</p>
+                <p>Rating: {ratings} Stars</p>
+            </div>
+            <button className='btn'>Add to Cart</button>
         </div>
     );
 };
